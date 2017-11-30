98b02a8517a87e9eea6bd093d97f152c1ea0489e
Kelsey Langlois and Megan Olson <kelsey+megan@>
2017-11-30 11:21:23 -0800

    Add popular categories scope.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.popular
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,10 @@ class Category < ActiveRecord::Base
   validates_uniqueness_of :name
 
   scope :alphabetical, -> { order("name ASC") }
+  scope :popular, -> {(
+    select("categories.id, categories.name, count(images.id) as images_count")
+    .joins(:images)
+    .group("categories.id")
+    .order("images_count DESC")
+    )}
 end
