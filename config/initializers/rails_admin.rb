f8b9934c34fecd8827ac8d4db83c8afb2cd85acc
Kelsey Langlois and Megan Olson <kelsey+megan@>
2017-11-30 14:25:41 -0800

    Update to not use CanCan for now.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -43,7 +43,12 @@ RailsAdmin.config do |config|
     warden.authenticate! scope: :user
   end
   config.current_user_method(&:current_user)
-  
-  config.authorize_with :cancan
+
+  config.authorize_with do |controller|
+    unless current_user.admin?
+      redirect_to main_app.root_path
+      flash[:alert] = "You are not an admin"
+    end
+  end
 
 end
