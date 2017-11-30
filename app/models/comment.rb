b16f17758e5b4d0613d5fd592f499bb1844ca1ec
Kelsey Langlois and Megan Olson <kelsey+megan@>
2017-11-30 11:11:15 -0800

    Add chronological scope to comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :image
   validates_presence_of :user, :image, :content
+
+  scope :chronological, -> { order("created_at ASC") }
 end
