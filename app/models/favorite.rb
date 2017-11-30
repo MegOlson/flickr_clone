99dada07911014cbff1f19c19d6f3ca7a1fd4460
Kelsey Langlois and Megan Olson <kelsey+megan@>
2017-11-30 11:45:09 -0800

    Add display of most recent favorites to profile.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,7 @@ class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :image
   validates_presence_of :image, :user
+
+  scope :recent, -> { order("created_at DESC").limit(3) }
+  
 end
